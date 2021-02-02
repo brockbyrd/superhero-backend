5a43e0ac153ecef954e4cbecc23530785e7fff20
brockbyrd <brockbyrdd@gmail.com>
2021-02-02 12:19:49 -0600

    creates function to create seed data from JSON data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,26 @@
 
 resp = HTTParty.get('https://akabab.github.io/superhero-api/api/all.json')
 data = JSON.parse(resp.body)
+
+data.each do |char|
+    Character.create(
+        name: char["name"],
+        intelligence: char["powerstats"]["intelligence"],
+        strength: char["powerstats"]["strength"],
+        speed: char["powerstats"]["speed"],
+        durability: char["powerstats"]["durability"],
+        power: char["powerstats"]["power"],
+        combat: char["powerstats"]["combat"],
+        race: char["appearance"]["race"],
+        gender: char["appearance"]["gender"],
+        height: char["appearance"]["height"][0],
+        weight: char["appearance"]["weight"][0],
+        fullName: char["biography"]["fullName"],
+        alignment: char["biography"]["alignment"],
+        occupation: char["work"]["occupation"],
+        groups: char["connections"]["groupAffiliation"],
+        universe: char["biography"]["publisher"],
+        image1: char["images"]["sm"],
+        image2: char["images"]["md"]
+    )
+end
