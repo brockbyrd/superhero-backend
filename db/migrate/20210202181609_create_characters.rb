134b36b2b641482628766fb0734588c28221cd0d
brockbyrd <brockbyrdd@gmail.com>
2021-02-02 12:18:09 -0600

    creates character tables

--- db/migrate/20210202181609_create_characters.rb
+++ db/migrate/20210202181609_create_characters.rb
@@ -1,6 +1,24 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
     create_table :characters do |t|
+      t.string :name
+      t.integer :intelligence
+      t.integer :strength
+      t.integer :speed
+      t.integer :durability
+      t.integer :power
+      t.integer :combat
+      t.string :race
+      t.string :gender
+      t.string :height
+      t.string :weight
+      t.string :fullName
+      t.string :alignment
+      t.string :occupation
+      t.string :groups
+      t.string :universe
+      t.string :image1
+      t.string :image2
 
       t.timestamps
     end
