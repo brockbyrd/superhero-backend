6417474fffbbf834dd7bb1eb6062aca608d75a76
brockbyrd <brockbyrdd@gmail.com>
2021-02-02 12:24:34 -0600

    exposes characters to index and renders json

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,7 @@
 class CharactersController < ApplicationController
+
+    def index
+        @characters = Character.all
+        render json: CharacterSerializer.new(@characters).serializeable_json
+    end
 end
