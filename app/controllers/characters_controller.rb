de90d8531d237edf11e2a08a490eefa72c8cd8c3
brockbyrd <brockbyrdd@gmail.com>
2021-02-12 10:43:48 -0600

    creates create route in characters controller

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,7 +1,20 @@
 class CharactersController < ApplicationController
 
     def index
-        @characters = Character.all
-        render json: CharacterSerializer.new(@characters).serializeable_json
+        characters = Character.all
+        render json: characters
     end
+
+    def create
+        character = Character.create(character_params)
+        render json: character
+    end
+
+    private
+
+    def character_params
+        params.require(:character).permit(:id, :name, :intelligence, :strength, :speed, :durability, :power, :combat, :race, :gender, :height, :weight, :fullName, :alignment, :occupation, :groups, :universe, :image1)
+    end
+
+
 end
